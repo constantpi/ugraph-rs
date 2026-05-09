548adc1ca964e5d6b338fb5d3c1c24617317aaa9
constantpi <wada.zkai@gmail.com>
2026-05-09 15:38:57 +0900

    射影を計算する

--- src/cad/proj.rs
+++ src/cad/proj.rs
@@ -1,3 +1,5 @@
+use crate::cad::matrix::{Matrix, generate_neg_list};
+use itertools::{Itertools, iproduct};
 use num::BigRational;
 
 use crate::polynomial::{Exponent, Polynomial};
@@ -19,9 +21,7 @@ fn collect_last_variable_coefficients(poly: &Polynomial) -> Option<Vec<Polynomia
     let mut coeffs = vec![Polynomial::zero(); (max_degree + 1) as usize];
     for (last, rest, coeff) in terms {
         let index = last as usize;
-        let mut coeff_poly = Polynomial::zero();
-        coeff_poly.add_term(rest, coeff);
-        coeffs[index] += coeff_poly;
+        coeffs[index].add_term(rest, coeff);
     }
 
     Some(coeffs)
@@ -44,4 +44,101 @@ fn is_constant_for_last_variable(poly: &Polynomial) -> bool {
     } else {
         true
     }
+}
+
+fn psc(f: &[Polynomial], g: &[Polynomial], l: usize) -> Polynomial {
+    let Some(f_degree) = f.len().checked_sub(1) else {
+        return Polynomial::zero();
+    };
+    let Some(g_degree) = g.len().checked_sub(1) else {
+        return Polynomial::zero();
+    };
+    if l >= f_degree || l >= g_degree {
+        return Polynomial::zero();
+    }
+    let f_rows = g_degree - l;
+    let g_rows = f_degree - l;
+    let size = f_rows + g_rows;
+    let mut matrix = Matrix::zero(size).unwrap();
+    for i in 0..f_rows {
+        for j in 0..=f_degree {
+            if size <= i + j {
+                break;
+            }
+            matrix.set(i, i + j, f[f_degree - j].clone());
+        }
+    }
+    for i in 0..g_rows {
+        for j in 0..=g_degree {
+            if size <= i + j {
+                break;
+            }
+            matrix.set(f_rows + i, i + j, g[g_degree - j].clone());
+        }
+    }
+
+    let neg_list = generate_neg_list(size);
+    matrix.determinant(&neg_list)
+}
+
+fn psc_list(f: &[Polynomial], g: &[Polynomial]) -> Vec<Polynomial> {
+    let min_degree = f.len().min(g.len());
+    (0..min_degree).map(|l| psc(f, g, l)).collect()
+}
+
+/// 多項式から射影を計算する関数
+pub fn project_polynomial(polys: &[Polynomial]) -> Vec<Polynomial> {
+    let (constants, non_constants): (Vec<Polynomial>, Vec<Polynomial>) = polys
+        .iter()
+        .cloned()
+        .partition(|p| is_constant_for_last_variable(p));
+    let mut projected = Vec::new();
+    for constant in constants {
+        projected.push(constant.constant_term().unwrap());
+    }
+
+    let coeff_lists: Vec<Vec<Polynomial>> = non_constants
+        .iter()
+        .filter_map(|p| collect_last_variable_coefficients(p))
+        .collect();
+
+    // proj1の計算
+    for coeffs in coeff_lists.iter() {
+        for coeff in differentiate_coefficients(&coeffs) {
+            projected.push(coeff);
+        }
+    }
+
+    // proj2の計算
+    for coeffs in coeff_lists.iter() {
+        let len = coeffs.len();
+        for i in 2..=len {
+            let f = &coeffs[..i];
+            let df = &differentiate_coefficients(f);
+            let pscs = psc_list(f, df);
+            projected.extend(pscs);
+        }
+    }
+
+    // proj3の計算
+    for (coeffs1, coeffs2) in coeff_lists.iter().tuple_combinations() {
+        let len1 = coeffs1.len();
+        let len2 = coeffs2.len();
+        for (i, j) in iproduct!(2..=len1, 2..=len2) {
+            let f = &coeffs1[..i];
+            let g = &coeffs2[..j];
+            let pscs = psc_list(f, g);
+            projected.extend(pscs);
+        }
+    }
+
+    // 重複を削除する
+    let mut unique_projected = Vec::new();
+    for p in projected {
+        if !unique_projected.contains(&p) {
+            unique_projected.push(p);
+        }
+    }
+
+    unique_projected
 }
