96bfd906c2d47c9d92cba98b834c4c5a59f1a02d
constantpi <wada.zkai@gmail.com>
2026-05-23 15:01:49 +0900

    CADアルゴリズム

---
+++ src/cad/algorithm.rs
@@ -0,0 +1,43 @@
+use color_eyre::Result;
+use vec1::Vec1;
+
+use super::{
+    Root, UnivariatePolynomial, find_all_roots, polynomial_to_univariate, project_polynomial,
+};
+use crate::polynomial::Polynomial;
+
+pub enum Solution {
+    NoSolution,
+    Exist(Vec<Root>),
+}
+
+pub fn find_solution(polinomials: &Vec<Polynomial>) -> Result<Solution> {
+    // 変数の数をまずは取得
+    let Some(num_vars) = polinomials
+        .iter()
+        .find_map(|p| p.raw_iter().next().map(|(exp, _)| exp.len()))
+    else {
+        return Ok(Solution::Exist(vec![])); // どの変数も出てこない場合は常に解が存在する
+    };
+    let mut current_num_vars = num_vars;
+    let mut current_polynomials = polinomials.clone();
+    let mut history = Vec::new();
+    while current_num_vars > 1 {
+        history.push(current_polynomials.clone());
+        current_num_vars -= 1;
+        current_polynomials = project_polynomial(&current_polynomials);
+    }
+    let univariate_polynomials = current_polynomials
+        .iter()
+        .map(polynomial_to_univariate)
+        .collect::<Result<Vec<_>>>()?;
+    let all_roots = univariate_polynomials
+        .iter()
+        .flat_map(|poly| find_all_roots(poly))
+        .collect::<Vec<_>>();
+    for root in all_roots {
+        println!("Checking root: {}", root);
+    }
+
+    todo!()
+}
--- src/cad/mod.rs
+++ src/cad/mod.rs
@@ -4,7 +4,8 @@ mod proj;
 mod sturm;
 mod univariate;
 
+pub use algorithm::find_solution;
 pub use proj::project_polynomial;
 use sturm::{Root, find_all_roots};
-pub use univariate::UnivariatePolynomial;
+pub use univariate::{UnivariatePolynomial, polynomial_to_univariate};
 use univariate::{uni_poly_derivative, uni_poly_div, uni_poly_remainder};
