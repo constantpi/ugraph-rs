548e2f4339fa770f48dbfda80c42ed5bf14e264e
constantpi <wada.zkai@gmail.com>
2026-05-23 21:52:24 +0900

    解ではなくサンプル点を求めることに注意

--- src/cad/lift.rs
+++ src/cad/lift.rs
@@ -1,6 +1,6 @@
 use color_eyre::{Result, eyre::eyre};
 
-use super::{Root, find_unique_roots, specialize_polynomial};
+use super::{Root, calc_sample_points, find_unique_roots, specialize_polynomial};
 use crate::polynomial::Polynomial;
 
 /// あるサンプル点に対して多変数多項式の解を見つける関数
@@ -18,11 +18,30 @@ fn find_solutions_at_sample_point(poly_list: &[Polynomial], sample: &[Root]) -> 
     Ok(find_unique_roots(&specialized_polys))
 }
 
+/// あるサンプル点からその直線上のサンプル点を生成する関数
+fn generate_sample_points_on_line(
+    poly_list: &[Polynomial],
+    sample: &[Root],
+) -> Result<Vec<Vec<Root>>> {
+    let solutions = find_solutions_at_sample_point(poly_list, sample)?;
+    let samples_on_line = calc_sample_points(&solutions);
+    let new_samples = samples_on_line
+        .into_iter()
+        .map(|solution| {
+            let mut new_sample = sample.to_vec();
+            new_sample.push(solution);
+            new_sample
+        })
+        .collect::<Vec<_>>();
+    Ok(new_samples)
+}
+
 /// CADのLiftステップを実装する関数
 /// あるサンプル点に対して、そこから上の次元の解を見つける関数
 pub fn lifting(poly_list: &[Polynomial], sample_list: &[Vec<Root>]) -> Result<Vec<Vec<Root>>> {
-    sample_list
+    let new_sample_list = sample_list
         .iter()
-        .map(|sample| find_solutions_at_sample_point(poly_list, sample))
-        .collect::<Result<Vec<_>>>()
+        .map(|sample| generate_sample_points_on_line(poly_list, sample))
+        .collect::<Result<Vec<_>>>()?;
+    Ok(new_sample_list.into_iter().flatten().collect())
 }
