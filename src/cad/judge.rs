e8b7d1545021f7ca51260c9c1579163b3bd9881e
constantpi <wada.zkai@gmail.com>
2026-05-24 15:59:55 +0900

    サンプルの精度を高めると0に向かうかどうかで解判定をする

--- src/cad/judge.rs
+++ src/cad/judge.rs
@@ -35,6 +35,14 @@ pub fn is_possible_solution(poly_list: &[Polynomial], sample: &[Root]) -> Result
     }
 }
 
+/// SignRelationからabsを取り出す関数
+fn get_cross_zero_abs(sign_relation: SignRelation) -> Option<BigRational> {
+    match sign_relation {
+        SignRelation::CrossZero(abs) => Some(abs),
+        _ => None,
+    }
+}
+
 /// 区間演算によってサンプル点が多項式の解であるかどうかを判定する関数
 pub fn is_possible_solution_interval(poly_list: &[Polynomial], sample: &[Root]) -> Solution {
     let mut sample = sample.to_vec();
@@ -44,19 +52,41 @@ pub fn is_possible_solution_interval(poly_list: &[Polynomial], sample: &[Root]) 
 
     for poly in poly_list {
         loop {
-            match evaluate_polynomial_at_signed_range(poly, &sample) {
-                SignRelation::Negative | SignRelation::Positive => return Solution::NoSolution, // 0をまたいでいない場合は解ではない
-                SignRelation::CrossZero(abs) => {
-                    // 0をまたいでいる場合は精度を高める
-                    if abs < threshold {
-                        break; // 十分に精度が高まっている場合はこれ以上精度を高めない
-                    }
-                    for root in sample.iter_mut() {
-                        if let Some(refined) = refine_root(root) {
-                            *root = refined;
-                        }
-                    }
+            let Some(abs) = get_cross_zero_abs(evaluate_polynomial_at_signed_range(poly, &sample))
+            else {
+                return Solution::NoSolution; // 0をまたいでいない場合は解ではない
+            };
+            if abs < threshold {
+                break; // 十分に精度が高まっている場合はこれ以上精度を高めない
+            }
+            for root in sample.iter_mut() {
+                if let Some(refined) = refine_root(root) {
+                    *root = refined;
                 }
+            }
+        }
+    }
+
+    let ratio = BigRational::from_integer(7.into()) / BigRational::from_integer(10.into());
+    for poly in poly_list {
+        let Some(abs) = get_cross_zero_abs(evaluate_polynomial_at_signed_range(poly, &sample))
+        else {
+            return Solution::NoSolution; // 0をまたいでいない場合は解ではない
+        };
+        let mut refined_sample = sample.clone();
+        for i in 0..10 {
+            for root in refined_sample.iter_mut() {
+                if let Some(refined) = refine_root(root) {
+                    *root = refined;
+                }
+            }
+            let Some(new_abs) =
+                get_cross_zero_abs(evaluate_polynomial_at_signed_range(poly, &refined_sample))
+            else {
+                return Solution::NoSolution; // 0をまたいでいない場合は解ではない
+            };
+            if new_abs > abs.clone() * (ratio.pow(i)) {
+                return Solution::NoSolution; // 誤差が十分に縮まっていない場合は解ではない
             }
         }
     }
