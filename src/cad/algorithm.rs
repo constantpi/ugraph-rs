687d387fd35cbd6a62c68cf75789198169b4ac71
constantpi <wada.zkai@gmail.com>
2026-06-03 22:26:06 +0900

    liftingする前に条件を満たさないサンプル点は排除しておく

--- src/cad/algorithm.rs
+++ src/cad/algorithm.rs
@@ -30,6 +30,23 @@ pub fn find_solution(ineqs: &[(Polynomial, RelOp)]) -> Result<Solution> {
     else {
         return Ok(Solution::Exist(vec![])); // どの変数も出てこない場合は常に解が存在する
     };
+    let ineqs_by_num_vars = {
+        let mut ineqs_by_num_vars = vec![Vec::new(); num_vars + 1];
+        for (poly, rel_op) in ineqs {
+            let poly_num_vars = poly.num_vars();
+            if poly_num_vars > num_vars {
+                return Err(color_eyre::eyre::eyre!(
+                    "Unexpected error: Polynomial has more variables than expected"
+                ));
+            }
+            ineqs_by_num_vars[poly_num_vars].push((poly.clone(), *rel_op));
+        }
+        ineqs_by_num_vars
+    };
+    // まず変数が0個のものは処理しておく
+    if is_solution_by_interval(&ineqs_by_num_vars[0], &vec![]) == Solution::NoSolution {
+        return Ok(Solution::NoSolution);
+    }
     let mut current_num_vars = num_vars;
     let mut current_polynomials = polinomials.to_vec();
     let mut history = Vec::new();
@@ -37,11 +54,12 @@ pub fn find_solution(ineqs: &[(Polynomial, RelOp)]) -> Result<Solution> {
         history.push(current_polynomials.clone());
         current_num_vars -= 1;
         current_polynomials = project_polynomial(&current_polynomials);
+        println!(
+            "Finished projection. Current number of variables: {}, number of projected polynomials: {}",
+            current_num_vars,
+            current_polynomials.len()
+        );
     }
-    println!(
-        "Finished projection. Number of projected polynomials: {}",
-        current_polynomials.len()
-    );
     let univariate_polynomials = current_polynomials
         .iter()
         .map(polynomial_to_univariate)
@@ -51,11 +69,41 @@ pub fn find_solution(ineqs: &[(Polynomial, RelOp)]) -> Result<Solution> {
         .iter()
         .map(|r| vec![r.clone()])
         .collect::<Vec<_>>();
+    println!("Sample points after projection: {}", sample_points.len());
     // historyを逆順にたどりながら、sample_pointsをliftingしていく
     for polynomials in history.into_iter().rev() {
-        println!("sample points before lifting: {}", sample_points.len());
+        // liftingの前に、sample_pointsがineqsを満たすかどうかを判定する
+        sample_points = sample_points
+            .into_iter()
+            .filter(|sample| {
+                matches!(
+                    is_solution_by_interval(&ineqs_by_num_vars[current_num_vars], sample),
+                    Solution::Exist(_)
+                )
+            })
+            .collect();
+        println!(
+            "sample points before lifting: {}, current_num_vars: {}",
+            sample_points.len(),
+            current_num_vars
+        );
         sample_points = lifting(&polynomials, &sample_points)?;
+        current_num_vars += 1;
+        println!(
+            "sample points after lifting: {}, current_num_vars: {}",
+            sample_points.len(),
+            current_num_vars
+        );
     }
+    sample_points = sample_points
+        .into_iter()
+        .filter(|sample| {
+            matches!(
+                is_solution_by_interval(&ineqs_by_num_vars[current_num_vars], sample),
+                Solution::Exist(_)
+            )
+        })
+        .collect();
     if sample_points.iter().any(|sample| sample.len() != num_vars) {
         return Err(color_eyre::eyre::eyre!(
             "Unexpected error: Sample points have incorrect number of variables"
